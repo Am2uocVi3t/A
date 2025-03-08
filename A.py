960159b6b5f5d607ee85b7fe5f5e924046ef092a
FappLord <phatno13252@gmail.com>
2025-03-08 16:08:41 +0700

    Update function f01

--- A.py
+++ A.py
@@ -3,7 +3,7 @@ from colorama import Fore, Style
 
 def f01(message):
     print(Fore.GREEN + message + Style.RESET_ALL)
-
+    print("Helooo world")
 
 def f02(message):
     print(Fore.RED + message + Style.RESET_ALL)
